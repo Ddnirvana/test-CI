dd7a0a71281075e8ab300bddbab4a9fa039958f0
Dong Du <dd_nirvana@sjtu.edu.cn>
2021-05-31 22:43:07 +0800

    add opensbi v0.9
    Signed-off-by: Dong Du <dd_nirvana@sjtu.edu.cn>

--- scripts/build_opensbi.sh
+++ scripts/build_opensbi.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd /home/penglai/penglai-enclave/opensbi-0.6
+cd /home/penglai/penglai-enclave/opensbi-0.9
 mkdir -p build-oe/qemu-virt
-CROSS_COMPILE=riscv64-unknown-linux-gnu- make O=build-oe/qemu-virt PLATFORM=qemu/virt FW_PAYLOAD=y FW_PAYLOAD_PATH=/home/penglai/penglai-enclave/Image
-#cp build-oe/qemu-virt/platform/qemu/virt/firmware/fw_payload.elf build-oe/qemu-virt/boot/fw_payload_oe_qemuvirt.elf
+CROSS_COMPILE=riscv64-unknown-linux-gnu- make O=build-oe/qemu-virt PLATFORM=generic FW_PAYLOAD=y FW_PAYLOAD_PATH=/home/penglai/penglai-enclave/Image
